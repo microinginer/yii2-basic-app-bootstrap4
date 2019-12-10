1ebb19458c9a6a84e0a768b46182700f199748ff
ruslanmadatov <ruslanmadatov@yandex.ru>
2019-12-10 18:36:28 +0500

    fix access control

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -20,10 +20,9 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout'],
                 'rules' => [
                     [
-                        'actions' => ['logout'],
+                        'actions' => ['logout','index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
